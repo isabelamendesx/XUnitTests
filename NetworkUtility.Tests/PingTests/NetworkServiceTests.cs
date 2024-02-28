e3f9870507c1c306e90f43f8c63b4ae5065febaf
isabelamendesx <gomesisabela13@gmail.com>
2024-02-28 13:07:18 -0300

    class 2: FluentAssertions and XUnit

---
+++ NetworkUtility.Tests/PingTests/NetworkServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using NetworkUtility.Ping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// NAME - ClassName_MethodName_ExpectedResult
+
+namespace NetworkUtility.Tests.PingTests
+{
+    public class NetworkServiceTests
+    {
+        [Fact]
+        public void NetworkService_SendPing_ReturnString()
+        {
+            // Arrange
+            var networkService = new NetworkService();
+
+            // Act
+            var result = networkService.SendPing();
+
+            // Assert 
+            result.Should().NotBeNullOrEmpty();
+            result.Should().Be("Sucess: Ping Sent!"); // Maybe too strict
+            result.Should().Contain("Sucess", Exactly.Once()); // Middle
+        }
+
+        [Theory]
+        [InlineData(2, 2, 4)]
+        public void NetworkService_PingTimeOut_ReturnInt(int a, int b, int expectedResult)
+        {
+            var networkService = new NetworkService();
+
+            var result = networkService.PingTimeOut(a, b);
+
+            result.Should().Be(expectedResult);
+            result.Should().NotBeInRange(-10000, 0);
+        }
+    }
+}
---
+++ NetworkUtility/Ping/NetworkService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkUtility.Ping
+{
+    public class NetworkService
+    {
+        public string SendPing()
+        {
+            return "Sucess: Ping Sent!";
+        }
+
+        public int PingTimeOut(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
